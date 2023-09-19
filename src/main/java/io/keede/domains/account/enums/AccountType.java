93a0b0b316bc280920e8348ee63f5546fc4966e0
keede <wesaq11@gmail.com>
2023-09-20 01:40:35 +0900

    feat : 계좌 종류 필터링 메세지 구현

--- src/main/java/io/keede/domains/account/enums/AccountType.java
+++ src/main/java/io/keede/domains/account/enums/AccountType.java
@@ -1,8 +1,14 @@
 package io.keede.domains.account.enums;
 
+import java.util.Arrays;
+
 public enum AccountType {
 
     NORMAL,
-    SAVINGS,
+    SAVINGS
+    ;
 
+    public boolean isMatchAccountType(AccountType accountType) {
+        return Arrays.asList(values()).contains(accountType);
+    }
 }
