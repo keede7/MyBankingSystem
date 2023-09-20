af445e2f48c810e46743320db9040181d4f13b17
keede <wesaq11@gmail.com>
2023-09-21 00:34:47 +0900

    feat: 이자율 정책 기능 구현

--- src/main/java/io/keede/domains/bank/policy/NormalAccountInterestCalculator.java
+++ src/main/java/io/keede/domains/bank/policy/NormalAccountInterestCalculator.java
@@ -10,14 +10,28 @@ import java.math.BigDecimal;
 */
 public class NormalAccountInterestCalculator implements InterestCalculator {
 
+    /**
+     * 일반 예금 계좌인 경우 1000만원 이상은 이자율이 50%, 500만원 이상은 7%, 100만원 이상은 4%, 1만원 이상은 2%, 그 외에는 1% 이자를 제공해줍니다.
+     */
     @Override
     public boolean match(AccountType accountType) {
-        return false;
+        return AccountType.NORMAL.isMatchAccountType(accountType);
     }
 
     @Override
     public BigDecimal getInterest(BigDecimal balance) {
-        return null;
-    }
+        long money = balance.longValue();
 
+        if(money >= 10_000_000L) {
+            return BigDecimal.valueOf(money * 0.5);
+        } else if(money >= 5_000_000L) {
+            return BigDecimal.valueOf(money * 0.07);
+        } else if(money >= 1_000_000L) {
+            return BigDecimal.valueOf(money * 0.04);
+        } else if(money >= 10_000L) {
+            return BigDecimal.valueOf(money * 0.02);
+        }
+
+        return BigDecimal.valueOf(money * 0.01);
+    }
 }
--- src/main/java/io/keede/domains/bank/policy/SavingsAccountInterestCalculator.java
+++ src/main/java/io/keede/domains/bank/policy/SavingsAccountInterestCalculator.java
@@ -10,14 +10,24 @@ import java.math.BigDecimal;
 */
 public class SavingsAccountInterestCalculator implements InterestCalculator {
 
+    /**
+     * 적금 계좌의 경우 100만원 이상은 이자율이 50%, 그 외에는 1% 이자를 제공해줍니다.
+     */
     @Override
     public boolean match(AccountType accountType) {
-        return false;
+        return AccountType.SAVINGS.isMatchAccountType(accountType);
     }
 
     @Override
     public BigDecimal getInterest(BigDecimal balance) {
-        return null;
+
+        long money = balance.longValue();
+
+        if(money >= 1_000_000L) {
+            return BigDecimal.valueOf(money * 0.5);
+        }
+
+        return BigDecimal.valueOf(money * 0.01);
     }
 
 }
